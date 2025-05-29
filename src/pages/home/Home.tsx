2662af4980f41bf573754b7300e015492ca18073
lyubo <l87r78@gmail.com>
2025-05-29 13:39:07 +0300

    Added home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,30 @@
-import { Box } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 import classes from './styles';
 
 const Home = () => {
-  return <Box sx={classes.layout}>Home</Box>;
+  return (
+    <Box sx={classes.layout}>
+      <Typography sx={classes.title} variant="h1">
+        Welcome, Maria
+      </Typography>
+      <Typography sx={classes.description} variant="h2">
+        Успешен ден
+      </Typography>
+      <Box sx={classes.wrapperWaitingBox}>
+        <Typography sx={classes.textWaitingPeople} variant="h2">
+          Чакащи
+        </Typography>
+        <Typography sx={classes.textWaitingPeople} variant="h2">
+          2
+        </Typography>
+      </Box>
+      <Box sx={classes.wrapperActions}>
+        <Button variant="contained"> Следваш клиент </Button>
+        <Button variant="outlined"> Стартирай обслужване </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,6 +1,38 @@
 const classes = {
   layout: {
     height: '100%',
+    marginTop: '10%',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: '40px',
+    color: 'var(--clr-text-label)',
+  },
+  description: {
+    textAlign: 'center',
+    fontSize: '24px',
+    color: 'var(--clr-text-label)',
+  },
+  wrapperWaitingBox: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '16px 24px',
+    width: '400px',
+    height: '60px',
+    margin: '24px auto',
+    background: '#F4F4F6',
+    borderRadius: '16px',
+  },
+  textWaitingPeople: {
+    fontSize: '18px',
+  },
+  wrapperActions: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '24px auto',
+    width: '296px',
+    gap: '16px',
   },
 };
 
